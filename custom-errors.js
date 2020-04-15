daa9e89b8cc3b1df27cc1420beccd7087ecec7be
i-sundutova <60006899+i-sundutova@users.noreply.github.com>
2020-04-15 16:22:56 +0300

    Create file for custom errors.

---
+++ custom-errors.js
@@ -0,0 +1,20 @@
+class ArgumentError extends Error {
+    constructor(message = "Value is not valid.") {
+        super(message);
+        this.name = "ArgumentError";
+    }
+}
+
+class ArgumentNullError extends ArgumentError {
+    constructor(message = "Value is null.") {
+        super(message);
+        this.name = "ArgumentNullError";
+    }
+}
+
+class ArgumentUndefinedError extends ArgumentError {
+    constructor(message = "Value is undefined.") {
+        super(message);
+        this.name = "ArgumentUndefinedError";
+    }
+}
