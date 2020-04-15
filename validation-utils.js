f8793684fc5ae3fbb3b1f3a897a666255a77bc38
i-sundutova <60006899+i-sundutova@users.noreply.github.com>
2020-04-15 16:21:45 +0300

    Created file with validation utils.

---
+++ validation-utils.js
@@ -0,0 +1,69 @@
+/**
+* Represents methods that can be used to ensure that parameter values meet expected conditions.
+*/
+class Ensure {
+    /**
+     * Ensures that the value of a parameter is not null.
+     * @throws ArgumentNullError
+     */
+    static isNotNull(value) {
+        if (value === null) throw new ArgumentNullError();
+    }
+
+    /**
+     * Ensures that the value of a parameter is not undefined.
+     * @throws ArgumentUndefinedError
+     */
+    static isNotUndefined(value) {
+        if (value === undefined) throw new ArgumentUndefinedError();
+    }
+
+    /**
+     * Ensures that the string value of a parameter is not null or empty.
+     * @param {string} value
+     * @throws ArgumentError
+     */
+    static isNotNullOrEmpty(value) {
+        if (value === null || value.length == 0) throw new ArgumentError("String value is null or empty.");
+    }
+
+    /**
+     * Ensures that one number value of a parameter greater then another.
+     * @param {number} value
+     * @param {number} comparand
+     * @throws ArgumentError
+     */
+    static isGreaterThan(value, comparand) {
+        if (value <= comparand) throw new ArgumentError(`${value} is not greater then ${comparand}.`);
+    }
+
+    /**
+     * Ensures that the number value of a parameter is greater then zero (0).
+     * @param {number} value
+     * @throws ArgumentError
+     */
+    static isGreaterThanZero(value) {
+        if (value <= 0) throw new ArgumentError(`${value} is not greater then zero(0).`);
+    }
+
+    /**
+     * Ensures that the value of a parameter is number (and not NaN).
+     * @param {number} value
+     * @throws ArgumentError
+     */
+    static isNumberAndNotNaN(value) {
+        if (typeof value !== 'number' || isNaN(value)) throw new ArgumentError(`${value} is NaN or not a number.`);
+    }
+
+    /**
+     * Ensures that the value of a parameter is in some range.
+     * @param {number} value
+     * @param {number} min
+     * @param {number} max
+     * @throws ArgumentError
+     */
+    static isBetween(value, min, max) {
+        this.isNumberAndNotNaN(value);
+        if (value < min || value > max) throw new ArgumentError(`${value} is not between ${min} and ${max}.`);
+    }
+}
